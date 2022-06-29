f8169461a64a8139f818d3b82b4550bee6cf6b00
mhaikalazizan <mhaikalazizan@gmail.com>
2022-06-30 00:00:41 +0800

    Use blade view feature to make view more manageable

--- app/Http/Controllers/Accounting/DashboardController.php
+++ app/Http/Controllers/Accounting/DashboardController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index(){
-        return view('layouts.app');
+        return view('dashboard.index');
     }
 }
