cd576e4c97c0cfdb3e2b00c3c4b827ee90e7d43f
Haikal Azizan <mhaikalazizan@gmail.com>
2022-07-01 09:35:04 +0800

    refactor(views): layouts/app.blade.php
    - modify copyright section with haikalazizan.com
    - move footer section from index to layouts/app

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -3,7 +3,6 @@
 @section('title', 'Dashboard')
     
     @section('content')
-        <div class="main-panel">
           <div class="content-wrapper pb-0">
             <div class="page-header flex-wrap">
               <div class="header-left">
@@ -578,20 +577,6 @@
             </div>
           </div>
           <!-- content-wrapper ends -->
-          <!-- partial:partials/_footer.html -->
-          <footer class="footer">
-            <div class="d-sm-flex justify-content-center justify-content-sm-between">
-              <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © bootstrapdash.com 2020</span>
-              <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Free <a href="https://www.bootstrapdash.com/" target="_blank">Bootstrap dashboard templates</a> from Bootstrapdash.com</span>
-            </div>
-
-            <div>
-              <span class="text-muted d-block text-center text-sm-left d-sm-inline-block"> Distributed By: <a href="https://themewagon.com/" target="_blank">Themewagon</a></span>
-            </div>
-          </footer>
-          <!-- partial -->
-        </div>
-        <!-- main-panel ends -->
     @stop
 
     @section('custom_script')
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -273,7 +273,22 @@
           </div>
         </nav>
         <!-- partial -->
-        @yield('content')
+        <div class="main-panel">
+          @yield('content')
+          <!-- partial:partials/_footer.html -->
+          <footer class="footer">
+            <div class="d-sm-flex justify-content-center justify-content-sm-between">
+              <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright &copy; haikalazizan.com 2022</span>
+              <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Free <a href="https://www.bootstrapdash.com/" target="_blank">Bootstrap dashboard templates</a> from Bootstrapdash.com</span>
+            </div>
+
+            <div>
+              <span class="text-muted d-block text-center text-sm-left d-sm-inline-block"> Distributed By: <a href="https://themewagon.com/" target="_blank">Themewagon</a></span>
+            </div>
+          </footer>
+          <!-- partial -->
+        </div>
+        <!-- main-panel ends -->
       </div>
       <!-- page-body-wrapper ends -->
     </div>
